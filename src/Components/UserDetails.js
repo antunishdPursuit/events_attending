b4e859d149873831b686b985538863dd5f1859b8
antunishdPursuit <dennysantunish@pursuit.org>
2023-08-20 18:32:55 -0400

    Completed the user detail componet and added the nessacery css

---
+++ src/Components/UserDetails.js
@@ -0,0 +1,57 @@
+import "../Css/UserDetailsCss.css"
+import axios from "axios";
+import UserCard from "./UserCard";
+// import Events from "./Events";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+const API = process.env.REACT_APP_API_URL;
+
+function UserDetails() {
+    const [user, setUser] = useState([]);
+    let navigate = useNavigate();
+    let { id } = useParams();
+ 
+    useEffect(() => {
+    axios
+      .get(`${API}/users/${id}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((c) => {
+        console.warn("catch", c);
+      });
+  }, [id]);
+
+  const handleDelete = () => {
+    deleteUser();
+  };
+
+  const deleteUser = () => {
+    axios
+      .delete(`${API}/users/${id}`)
+      .then(
+        () => {
+          navigate(`/users`);
+        },
+        (error) => console.error(error)
+      )
+      .catch((c) => console.warn("catch", c));
+  };
+
+  return (
+    <div className="row">
+        <div className="col-md-5">
+            <div className="card mb-3 col-md-5ol-4">
+                <UserCard user={user} id={id} handleDelete={handleDelete}/>
+                    {/* <Events/> */}
+            </div>
+        </div>
+        <div className="col-md-7">
+            <h1>ah</h1>
+        </div>
+    </div>
+  );
+}
+
+export default UserDetails;
+
--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,10 +1,10 @@
-// import UserDetails from "../Components/UserDetails";
+import UserDetails from "../Components/UserDetails";
 
 function Show() {
     return (
-        <div>
-            <h2>Show Page</h2>
-            {/* <UserDetails /> */}
+        <div className="container">
+            <br></br>
+            <UserDetails />
         </div>
     );
 }
