ef54931fbf95d8c6cf764a342b4743ea5d8917b1
antunishdPursuit <dennysantunish@pursuit.org>
2025-06-17 10:51:25 -0400

    Github Pages

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -3,12 +3,7 @@ import Users from "../Components/Users";
 
 function Index() {
     return (
-        <div className="container">
-            <div className="row align-items-center">
-                <div className="col user-box d-flex justify-content-center align-items-center">
-                    <h1>Users</h1>
-                </div>
-            </div>
+        <div className="Users">
             <Users />
         </div>
     );
